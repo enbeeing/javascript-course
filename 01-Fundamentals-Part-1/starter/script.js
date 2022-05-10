fa2332fd795323c2149700e69d238d15ee7d580a
enbeeing <eddykjllr@hotmail.com>
2022-05-10 08:49:35 +0200

    test

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,29 @@
-console.log("hello");
+'use strict';
+
+// variables
+
+let markHeight = 1.69, markMass = 78;
+
+// markHeight = 1.88, markMass = 95;
+
+let johnHeight = 1.95, johnMass = 92;
+
+// johnHeight = 1.76, johnMass = 85;
+
+const markBMI = getBMIResult(markHeight,markMass);
+
+const johnBMI = getBMIResult(johnHeight,johnMass);
+
+const markHigherBMI = markBMI > johnBMI;
+
+
+// functions
+
+function getBMIResult(height, mass){
+    return mass / (height * height);
+}
+
+
+// console logs
+
+console.log(markHigherBMI);
