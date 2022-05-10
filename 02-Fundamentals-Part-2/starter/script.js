a8d2afdcc2d3fad032239c0deebb2a467a9dc9a1
enbeeing <eddykjllr@hotmail.com>
2022-05-10 11:05:59 +0200

    challange 1

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,40 @@
 'use strict';
+
+// challange 1
+
+let dolphinsScores = [44,23,71];
+dolphinsScores = [85,54,41];
+
+let koalasScores = [65,54,49];
+koalasScores = [23,34,27];
+
+const calcAverage = (arr) => 
+    ([...arr].reduce((a,b) => a + b, 0)) / [...arr].length;
+
+const dolphinsAverage = calcAverage(dolphinsScores);
+
+const koalasAverage = calcAverage(koalasScores);
+
+function checkWinner(avgDolphins, avgKoalas){
+    let winner, winnerScore, loserScore;
+
+    if(avgDolphins > avgKoalas && avgDolphins >= 2 * avgKoalas){
+        winner = "Dolphins"
+        winnerScore = avgDolphins;
+        loserScore = avgKoalas;
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        winner = "Koalas"
+        winnerScore = avgKoalas;
+        loserScore = avgDolphins;
+    }
+
+    if(winner){
+        console.log(`${winner} win (${winnerScore} vs. ${loserScore})`);
+    } else {
+        console.log("No ones win");
+    }
+}
+
+checkWinner(dolphinsAverage, koalasAverage);
+
+
