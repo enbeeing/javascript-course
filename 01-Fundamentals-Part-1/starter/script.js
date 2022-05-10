26e28463132988cadcbc5d9191d7a769e54206f6
enbeeing <eddykjllr@hotmail.com>
2022-05-10 09:45:52 +0200

    challenge 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,14 +1,12 @@
 'use strict';
 
-// variables
+// challange 1
 
 let markHeight = 1.69, markMass = 78;
-
-// markHeight = 1.88, markMass = 95;
+markHeight = 1.88, markMass = 95;
 
 let johnHeight = 1.95, johnMass = 92;
-
-// johnHeight = 1.76, johnMass = 85;
+johnHeight = 1.76, johnMass = 85;
 
 const markBMI = getBMIResult(markHeight,markMass);
 
@@ -17,13 +15,67 @@ const johnBMI = getBMIResult(johnHeight,johnMass);
 const markHigherBMI = markBMI > johnBMI;
 
 
-// functions
-
 function getBMIResult(height, mass){
     return mass / (height * height);
 }
 
-
-// console logs
-
 console.log(markHigherBMI);
+
+// challange 2
+
+if(markBMI > johnBMI){
+    console.log(`Mark's BMI (${markBMI.toFixed(2)}) is higher than John's (${johnBMI.toFixed(2)})!`);
+} else{
+    console.log(`John's BMI (${johnBMI.toFixed(2)}) is higher than Marks's (${markBMI.toFixed(2)})!`);
+}
+
+// challange 3
+
+let dolphinsScores = [96,108,89];
+dolphinsScores = [97,112,101];
+dolphinsScores = [97,112,101];
+
+let koalasScores = [88,91,110];
+koalasScores = [109,95,123];
+koalasScores = [109,95,106];
+
+const dolphinsAverage = getAverageScore(dolphinsScores);
+const koalasAverage = getAverageScore(koalasScores);
+
+let winner = "";
+
+function getAverageScore(arr){
+    const sum = [...arr].reduce((a,b) => a + b, 0);
+
+    return sum / [...arr].length;
+}
+
+if(dolphinsAverage < 100 || koalasAverage < 100){
+    winner = "No teams average's over 100";
+} else {
+    if(koalasAverage === dolphinsAverage){
+        winner = "Draw";
+    } else if (koalasAverage > dolphinsAverage){
+        winner = `Koalas average is highest (${koalasAverage.toFixed(2)})`;
+    } else {
+        winner = `Dolphins average is highest (${dolphinsAverage.toFixed(2)})`;
+    }
+}
+
+console.log(winner);
+
+// challange 4
+
+let bill = 275;
+bill = 40;
+bill = 430;
+
+const tip = getTip(bill);
+
+const totalValue = bill + tip;
+
+function getTip(bill){
+    return bill * ((bill >= 50 && bill <= 300 ? 15 : 20) / 100);
+}
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`);
--- 04-HTML-CSS/final/script.js
+++ 04-HTML-CSS/final/script.js
@@ -1,15 +1,2 @@
 'use strict';
 
-let bill = 275;
-bill = 40;
-bill = 430;
-
-const tip = getTip(bill);
-
-const totalValue = bill + tip;
-
-function getTip(bill){
-    return bill * ((bill >= 50 && bill <= 300 ? 15 : 20) / 100);
-}
-
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`);
