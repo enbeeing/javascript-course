2b790b2e061fb48ba40d4993828cf13f7245b722
enbeeing <eddykjllr@hotmail.com>
2022-05-11 13:47:00 +0200

    deleted unused files, and started 05 clallange 1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// const body = document.querySelector('body');
+// const checkBtn = body.querySelector('.check');
+// const againBtn = body.querySelector('.again');
+
+// checkBtn.addEventListener('click', );
+
+// function checkNumber(){
+
+// }
