461d603d7a3bdbb2e49c5ce13e05544a124162d4
enbeeing <eddykjllr@hotmail.com>
2022-05-11 15:01:48 +0200

    05 basic game logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,11 +1,44 @@
 'use strict';
 
-// const body = document.querySelector('body');
-// const checkBtn = body.querySelector('.check');
-// const againBtn = body.querySelector('.again');
+// dom elements
+const body = document.querySelector('body');
+// btns
+const checkBtn = body.querySelector('.check');
+const againBtn = body.querySelector('.again');
+const displayMessage = body.querySelector('.message');
 
-// checkBtn.addEventListener('click', );
+// messages to player
+const correctMessage = `🎉 Correct number`;
+// const notCorrectMessage = `❌ Try again`;
+const noValueMessage = `🛑 No number!`;
+const tooHighMessage = `📈 Too high!`;
+const tooLowMessage = `📉 Too low!`;
 
-// function checkNumber(){
+// random numbergenerater 
+const randomNumberBetween = (min, max) => Math.trunc(Math.random() * max) + min;
 
-// }
+// game rules
+const correctNumber = randomNumberBetween(1,20);
+let guessInput, isWin;
+
+//remove this when game is done!!
+body.querySelector('.number').textContent = correctNumber;
+
+// btn eventlisteners
+checkBtn.addEventListener('click', checkNumber);
+
+// btn functions
+function checkNumber(){
+    guessInput = body.querySelector('.guess').value;
+    isWin = correctNumber == guessInput;
+
+    if(!guessInput){
+        displayMessage.textContent = noValueMessage;
+    } else if (isWin){
+        displayMessage.textContent = correctMessage;
+    } else if (guessInput > correctNumber){
+        displayMessage.textContent = tooHighMessage;
+    } else if (guessInput < correctNumber){
+        displayMessage.textContent = tooLowMessage;
+    }
+}
