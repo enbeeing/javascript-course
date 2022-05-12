2a0047cff24a2e30d50c5d82ae3264f0c3c65aae
enbeeing <eddykjllr@hotmail.com>
2022-05-12 10:57:53 +0200

    guess my number (win, lose, reset)

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -6,39 +6,104 @@ const body = document.querySelector('body');
 const checkBtn = body.querySelector('.check');
 const againBtn = body.querySelector('.again');
 const displayMessage = body.querySelector('.message');
+const scoreDisplay = body.querySelector('.score');
+const highscoreDisplay = body.querySelector('.highscore');
+const numberDisplay = body.querySelector('.number');
 
 // messages to player
-const correctMessage = `🎉 Correct number`;
-// const notCorrectMessage = `❌ Try again`;
-const noValueMessage = `🛑 No number!`;
+const defaultMessage = "Start guessing...";
+const correctMessage = `🎉 Correct number!`;
+const noValueMessage = `⛔ No number!`;
 const tooHighMessage = `📈 Too high!`;
 const tooLowMessage = `📉 Too low!`;
+const gameOverMessage = `💥 You lost the game!`;
 
 // random numbergenerater 
 const randomNumberBetween = (min, max) => Math.trunc(Math.random() * max) + min;
 
 // game rules
-const correctNumber = randomNumberBetween(1,20);
-let guessInput, isWin;
-
-//remove this when game is done!!
-body.querySelector('.number').textContent = correctNumber;
+const defaultScore = 20;
+let correctNumber = randomNumberBetween(1,20), score = defaultScore;
+let isWin = false, isGameOver = false;
+let guessInput, highscore;
 
 // btn eventlisteners
 checkBtn.addEventListener('click', checkNumber);
+againBtn.addEventListener('click', resetGame);
 
 // btn functions
 function checkNumber(){
     guessInput = body.querySelector('.guess').value;
-    isWin = correctNumber == guessInput;
+    isWin = correctNumber === Number(guessInput);
 
-    if(!guessInput){
-        displayMessage.textContent = noValueMessage;
-    } else if (isWin){
-        displayMessage.textContent = correctMessage;
-    } else if (guessInput > correctNumber){
-        displayMessage.textContent = tooHighMessage;
-    } else if (guessInput < correctNumber){
-        displayMessage.textContent = tooLowMessage;
+    if(!isGameOver){
+        if(!guessInput){
+            displayMessage.textContent = noValueMessage;
+        } else if (isWin){
+            playerWins();
+        } else if (score > 1){
+            if (guessInput > correctNumber){
+                displayMessage.textContent = tooHighMessage;
+            } else if (guessInput < correctNumber){
+                displayMessage.textContent = tooLowMessage;
+            }
+            score--;
+        } else {
+            gameOver();
+        }
     }
+    scoreDisplay.textContent = score;
+}
+
+function resetGame(){
+    if(isWin){
+        body.classList.remove('win');
+        numberDisplay.classList.remove('win');
+        console.log('win reset');
+    } 
+    if(isGameOver){
+        body.classList.remove('lose');
+        numberDisplay.classList.remove('lose');
+        console.log('lose reset');
+    }
+
+    // set and display default values
+    score = defaultScore;
+    scoreDisplay.textContent = score;
+    numberDisplay.textContent = "?";
+    displayMessage.textContent = defaultMessage;
+
+    isWin = false, isGameOver = false;
+    // give new number
+    correctNumber = randomNumberBetween(1,20);
+}
+
+// win and lose funtions
+
+function playerWins(){
+    displayMessage.textContent = correctMessage;
+    numberDisplay.textContent = correctNumber;
+
+    // set and display highscore
+    if(!highscore){
+        highscore = score;
+    } else if (highscore < score) {
+        highscore = score;
+    }
+    highscoreDisplay.textContent = highscore;
+
+    // add class and style in css
+    body.classList.add('win');
+    numberDisplay.classList.add('win');
+}
+
+function gameOver(){
+    displayMessage.textContent = gameOverMessage;
+    numberDisplay.textContent = correctNumber;
+    score--;
+    isGameOver = true;
+
+    // add class and style in css
+    body.classList.add('lose');
+    numberDisplay.classList.add('lose');
 }
