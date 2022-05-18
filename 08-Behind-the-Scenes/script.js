a5571482d36fc4d564dc1dbf5ba984f5dbe54730
enbeeing <eddykjllr@hotmail.com>
2022-05-18 14:39:57 +0200

    stuff

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,161 @@
 'use strict';
+
+// // hoisted and TDZ
+// console.log(me);
+// console.log(str());
+
+// var me = 'Jonas';
+// function str(){
+//     return 'str';
+// }
+
+// let job = 'teacher';
+// const year = 1991;
+// const str2nd = () => '2nd str';
+
+
+// console.log(job);
+// console.log(year);
+// console.log(str2nd());
+
+// console.log(me === window.me);
+// console.log(job === window.job);
+
+
+// // just testing different possibilities with objects
+let currentYear = 2022;
+
+// class Person {
+//     constructor(name, year) {
+//         this.name = name;
+//         this.year = year;
+//         this.calcAge = function () {
+//             return currentYear - this.year;
+//         };
+//     }
+// }
+// const jonas = new Person('Jonas',1992);
+// console.log(jonas.calcAge());
+
+// function Human (name, year) {
+//     this.name = name;
+//     this.year = year;
+//     this.calcAge = function(){
+//         return currentYear - this.year;
+//     }
+// }
+// const ash = new Human('Ash', 1998);
+// console.log(ash.name, ash.year, ash.calcAge());
+
+// const chris = {
+//     name: 'Chris',
+//     year: 1990,
+//     calcYear: function(){
+//         return currentYear - this.year;
+//     }
+// }
+// console.log(chris.calcYear());
+
+// // this
+// console.log('this:');
+
+// console.log(this);
+
+// const calcAge = function(year){
+//     console.log(currentYear - year);
+//     console.log(this);
+// }
+// calcAge(1997);
+
+// const calcAgeArrow = (year) => {
+//     console.log(currentYear - year);
+//     console.log(this);
+// }
+// calcAgeArrow(1997);
+
+// const jon = {
+//     year: 1991,
+//     calcAge: function(){
+//         console.log(this);
+//         console.log(currentYear - this.year);
+//     }
+// };
+// jon.calcAge();
+
+// const matilda = {
+//     year: 2000,
+// };
+// matilda.calcAge = jon.calcAge;
+// matilda.calcAge();
+
+// const f = jon.calcAge;
+// f();
+
+
+// // regular vs. arrow functions: this
+// const jon = {
+//     firstName: 'Jon',
+//     year: 1991,
+//     calcAge: function(){
+//         console.log(this);
+//         console.log(currentYear - this.year);
+
+//         // solution 1
+//         // const self = this; 
+//         // const isMillenial = function(){
+//         //     console.log(self.year >= 1981 && self.year <= 1996);
+//         // };
+//         // solution 2
+//         const isMillenial = () => console.log(this.year >= 1981 && this.year <= 1996);
+//         isMillenial();
+//     },
+//     greet: () => console.log(`Hi ${jon.firstName}`),
+// };
+// jon.greet();
+// jon.calcAge();
+
+// // arguments not in arrow
+// const addExpr = function(a,b){
+//     console.log(arguments);
+//     return a + b;
+// };
+// addExpr(2,5);
+// addExpr(2,5,8,12);
+
+// var addArrow = (a,b) => {
+//     // console.log(arguments);
+//     return a + b
+// };
+// addArrow(2,5);
+
+// primitives vs objects (primitives vs reference types)
+// primitive
+let age = 30;
+let oldAge = age;
+age++;
+console.log(age, oldAge);
+// reference
+const me = {
+    name: 'Edd',
+    age: 24,
+};
+const friend = me;
+friend.age = 27;
+console.log(me,friend);
+
+// primitive
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName,oldLastName);
+// copy object (not deep clone)
+const missJessica = {
+    firstName: 'Jessica',
+    lastName: 'Williams',
+    age: 27,
+    family: ['Alice', 'Bob']
+};
+const mrsJessica = Object.assign({}, missJessica);
+mrsJessica.lastName = 'Davis';
+mrsJessica.family.push('Mary');
+console.log(missJessica, mrsJessica);
--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -26,4 +26,161 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDeivery: function({
+    starterIndex = 2,
+    mainIndex = 0,
+    time = '19:00',
+    address,}){
+    console.log(`Order received! 
+    ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+    wil be delivered to ${address} at ${time}`);
+  },
+
+  orderPasta: function(ing1,ing2,ing3){
+    console.log(`here's your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza: function(mainIng, ...otherIng){
+    let pizzaOrder = `A pizza with ${mainIng}`;
+    otherIng.forEach(ing => {
+      pizzaOrder += `, ${ing}`
+    });
+    console.log(pizzaOrder);
+  },
 };
+
+// logical operators: use, return any datatype, short-circuting
+console.log(`---- OR ----`);
+console.log( 3 || 'Jon');
+console.log('' || 'Jon');
+console.log(true || 0);
+console.log(false || 0);
+console.log(0 || false);
+console.log(undefined || null);
+console.log(null || undefined);
+console.log(undefined || 0 || null || '' || 4);
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+console.log(`---- AND ----`);
+
+// // rest function
+// const add = function(...numbers){
+//   let sum = 0;
+//   numbers.forEach(num => {
+//     sum += num;
+//   });
+//   console.log(sum);
+// };
+// add(2,3);
+// add(5,3,7,2);
+// add(8,2,5,3,2,1,4);
+// const x = [23,5,7];
+// add(...x);
+
+// restaurant.orderPizza('onion', 'pineapple', 'peppers', 'artichoke','mushroom', 'corn');
+// restaurant.orderPizza('pineapple');
+
+// spread (on right side of =)
+// const arr = [1,2, ...[3,4]];
+// // rest (on left side of =)
+// const [a,b,...others] = [1,2,3,4,5];
+// console.log(a,b,others);
+
+// const [pizza,,Risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// console.log(pizza,Risotto,otherFood);
+// in obj
+// const {sat, ...weekdays} = restaurant.openingHours;
+// console.log(weekdays);
+
+// // spread 
+// const arr = [7,8,9];
+
+// const newArr = [1,2, ...arr];
+// console.log(newArr, ...newArr);
+
+// const newMenu = [...restaurant.mainMenu,'Gnocci'];
+// console.log(newMenu);
+
+// // copy arr
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// // join 2 arr
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+// // iterable: array, string, map, set. NOT obj
+// const strABC = 'abcdefghijklmnopqrstuvwxyzæøå';
+// const lettersABC = [...strABC];
+// console.log(strABC);
+// console.log(...strABC);
+
+// // const ingredients = [prompt(`Let's make pasta! Ingredient 1?`), 
+// // prompt(`Ingredient 2?`), 
+// // prompt(`Ingredient 3?`)];
+// // console.log(ingredients);
+// // restaurant.orderPasta(...ingredients);
+// // objects
+// const newResturant = {foundedIn: 1998, ...restaurant, founder: 'Guiseppe'};
+// console.log(newResturant);
+
+// const  resturantCopy = {...restaurant};
+// resturantCopy.name = 'Ristorante Roma';
+// console.log(restaurant.name, resturantCopy.name);
+
+// // destructureing objects
+// restaurant.orderDeivery({
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
+
+// restaurant.orderDeivery({
+//   address: 'Via del Sole, 21',
+// });
+
+// const {name, openingHours} = restaurant;
+// console.log(name, openingHours);
+// const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+// console.log(restaurantName, hours, tags);
+// // default values
+// const {menu: menu = [], starterMenu: starters = []} = restaurant;
+// console.log(menu, starters);
+// // mutating variables
+// let e = 111;
+// let t = 999;
+// const obj = {e: 23, t: 7, u:14};
+// ({e,t} = obj);
+// console.log(e,t);
+// // nested obj
+// const {openingHours: {fri: {open, close}}} = restaurant;
+// console.log(open,close);
+
+// //destructuring arrays
+// // switching varibles
+// let [,,x,y] = restaurant.categories;
+// console.log(x, y);
+// [x,y] = [y,x];
+// console.log(x,y)
+
+// // recieve 2 return values from function
+// const [starter, main] = restaurant.order(2,0);
+// console.log(starter, main);
+
+// // nested destructuring
+// const nested = [2,4,[5,6]];
+// const [i, ,j] = nested;
+// console.log(i,j);
+// const [a,,[b,console.log('');]] = nested;
+// console.log(a,b,console.log(''););
+
+// // default values
+// const[p = 1,q = 1,r = 1] = [8,9];
+// console.log(p,q,r);
